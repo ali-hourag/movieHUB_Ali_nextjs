b0bc8c1bc6fb0b419d421a8f744e3fc26445d8a4
Ali Hourag <ali.hourag@gmail.com>
2023-10-02 10:30:47 +0200

    feat(login): migrate login page to next

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -1,13 +1,16 @@
+import { SiThemoviedatabase } from 'react-icons/si';
+import styles from './login.module.css'
 
-
-type Props = {}
-
-const Login = (props: Props) => {
+const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
-            <a href="/api/auth/login">Login</a>
+        <div className={styles.container}>
+            <SiThemoviedatabase className={styles.logo} />
+            <button className={styles.btn}><a href="/api/auth/login" className={styles.anchorTag}>Log In</a></button>
         </div>
+        // <div>
+        //     <h1>Login</h1>
+        //     <a href="/api/auth/login">Login</a>
+        // </div>
     )
 }
 
